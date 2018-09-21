bff23f90165867c9da022e2f12db7c2e9721bb0b
Tamer Bader <italktechs@gmail.com>
2018-09-21 13:33:28 -0400

    Updated Project

--- Sep21Exercises/Communication/FirstVC.swift
+++ Sep21Exercises/Communication/FirstVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstVC.swift
 //  ProtocolExample
 //
 //  Created by Tamer Bader on 9/21/18.
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
     @IBAction func didTapNext(_ sender: UIButton) {
         
     }
     
+    
 }
-

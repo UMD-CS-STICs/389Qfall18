cad18992d26be04f709cc55973f7217c2978d138
Tamer Bader <italktechs@gmail.com>
2018-09-21 13:29:51 -0400

    Added Blank Segue/Comm Project

---
+++ Sep21Exercises/Communication/ProtocolExample/SecondVC.swift
@@ -0,0 +1,28 @@
+//
+//  SecondVC.swift
+//  ProtocolExample
+//
+//  Created by Tamer Bader on 9/21/18.
+//  Copyright © 2018 TamerBader. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func didTapGoBack(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+}
+
+
+
---
+++ Sep21Exercises/Communication/ProtocolExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProtocolExample
+//
+//  Created by Tamer Bader on 9/21/18.
+//  Copyright © 2018 TamerBader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func didTapNext(_ sender: UIButton) {
+        
+    }
+    
+}
+

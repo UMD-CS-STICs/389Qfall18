abe4a38aa74447539d2ede6b0a394ee1a6b6d710
Travis Ho <travisho@Traviss-MacBook-Pro.local>
2018-09-21 13:51:46 -0400

    add text field exercise

---
+++ Sep21Exercises/AutoSum/AutoSum/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AutoSum
+//
+//  Created by Travis Ho on 9/21/18.
+//  Copyright © 2018 Travis Ho. All rights reserved.
+//
+
+import UIKit
+
+// extend UITextFieldDelegate
+class ViewController: UIViewController {
+    
+    //Drag outlets for text field, and both display labels here
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // set UITextFieldDelegate
+        // add selector target for textFieldDidChange
+    }
+    
+    /*
+    @objc func textFieldDidChange(textField: UITextField){
+        // update labels
+    }
+     */
+    
+    
+}
+
